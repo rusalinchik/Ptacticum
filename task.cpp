05a6903c600d87345a00e1efce08757e7b1d5ada
verenahu <rusalinchikk@yandex.ru>
2025-12-04 00:45:44 +0300

    #5

--- task.cpp
+++ task.cpp
@@ -1,4 +1,7 @@
-﻿int max(const int* arr, int size) {
+﻿#include <iostream>
+using namespace std;
+
+int max(const int* arr, int size) {
     int maxVal = arr[0];
     for (int i = 1; i < size; i++) {
         if (arr[i] > maxVal) {
@@ -6,4 +9,24 @@
         }
     }
     return maxVal;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество чисел: ";
+    cin >> n;
+
+    int* arr = new int[n];
+
+    cout << "Введите числа:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maximum = max(arr, n);
+    cout << "Максимум: " << maximum << endl;
+
+    delete[] arr;
+    return 0;
 }
