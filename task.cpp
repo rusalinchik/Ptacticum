24f11f3362bef93dcca8a67940d92993ee1cc1f6
verenahu <rusalinchikk@yandex.ru>
2025-12-04 00:21:50 +0300

    #1

---
+++ task.cpp
@@ -0,0 +1,9 @@
+﻿int max(const int* arr, int size) {
+    int maxVal = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
